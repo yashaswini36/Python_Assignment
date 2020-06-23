8d9c8d2dece15c7e11aa4b35853eb0b4232ccece
yashaswini36 <67336256+yashaswini36@users.noreply.github.com>
2020-06-23 23:15:23 +0530

    Add files via upload

---
+++ perimeterofsquare.py
@@ -0,0 +1,3 @@
+s=int(input("Enter Side of square : "))
+perimeter=4*s
+print("Perimeter of square: ",perimeter)
---
+++ simple interest.py
@@ -0,0 +1,5 @@
+P = float(input("Enter the principal amount : "))
+N = float(input("Enter the number of years : "))
+R = float(input("Enter the rate of interest : "))
+SI = (P * N * R)/100
+print("Simple interest : {}".format(SI))
